f65c04edfbb10eceaf16eeb21e4fd6af5bbe5b3a
d0cdilip <sharmadilip9849@gmail.com>
2022-12-01 16:52:33 +0545

    pls check and send me feedback

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace assignment
+{
+    public static void main()
+    {
+        public class realworld {
+            public int age;
+            public int num;
+            private int test;
+            internal int alpha;
+        static void method1()
+            {
+                return;
+
+            }
+            
+            static void method2()
+            {
+                return;
+
+            }
+
+            
+        }
+            
+        public class another {
+            realworld obj1 = new realworld(); // assignment of object
+
+            obj1.age = 1;
+            obj1.num = 2;
+            //obj1.field3 = 3;
+            //obj1.field4 = 4;
+            realworld obj2 = new realworld(); //assignment of object
+
+            obj2 field1 = 5;
+            obj2 field2 = 6;
+            //obj1.field3 = 7;
+            //obj1.field4 = 8;
+
+        }
+    }   
+    
+}
