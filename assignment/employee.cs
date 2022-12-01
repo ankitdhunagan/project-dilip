f9b5aed467eabe72e0923ce3b6245a8e14d672a8
d0cdilip <sharmadilip9849@gmail.com>
2022-12-01 17:41:58 +0545

    added new file employee

---
+++ assignment/employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace assignment
+{
+
+        public class employee 
+        {
+            public string? name;
+            public string? dept;
+            private int num;
+            internal string? task;
+            public static void Main()
+            {
+                employee obj1 = new employee(); // assignment of object
+                obj1.name = "Ankit";
+                obj1.dept = "I.T";
+                obj1.num = 2;
+                obj1.task = "Responsible for all the I.T activities";
+
+                employee obj2 = new employee(); // assignment of object
+                obj2.name = "Priya";
+                obj2.dept = "P.M.S";
+                obj2.num = 3;
+                obj2.task = "Responsible for Management of portfolio of atleast 50 clients";
+
+                System.Console.WriteLine("Employee Name :" + " " + obj1.name + "," + " " + "Department :" + " " + " " + obj1.dept);
+               
+                System.Console.WriteLine("Employee Name :" + " " + obj2.name + "," + " " + "Department :" + " " + " " + obj2.dept);
+                           
+            }
+            
+        }
+}
