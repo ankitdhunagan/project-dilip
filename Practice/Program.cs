167a243645c179a0d4a6fcd6f6142e6b2e7d66df
ad1995bs <114225958+ad1995bs@users.noreply.github.com>
2022-12-04 11:45:28 +0545

    moved

---
+++ Practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Ankit
+
+    {
+        public static void Main()
+        {
+           /* Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("your age is:" + age);*/
+
+            Console.WriteLine ("Enter your age:");
+            string age = Console.ReadLine();
+            if age> 15
+            {
+                Console.WriteLine("You are a voter");
+            }
+            else
+            {
+                Console.WriteLine("You are a non voter");
+            }
+
+
+
+            return;  
+        }
+    }
