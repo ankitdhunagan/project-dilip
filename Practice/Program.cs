bfbfbf67506e25e04b5b1eb12a3854dc40c5206c
d0cdilip <sharmadilip9849@gmail.com>
2022-12-04 12:45:55 +0545

    new practise

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -10,14 +10,15 @@
             Console.WriteLine("your age is:" + age);*/
 
             Console.WriteLine ("Enter your age:");
-            string age = Console.ReadLine();
-            if (age => 18)
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your age is: " + age + " and are a voter");
+            if (age >= 18)
             {
-                Console.WriteLine("Your age is: "+ age + "and are a voter");
+                Console.WriteLine("Your age is: " + age + " and are a voter");
             }
             else
             {
-                Console.WriteLine("Your age is: "+ age + "and are not a voter");
+                Console.WriteLine("Your age is: " + age + " and are not a voter");
             }
 
 
---
+++ Practise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class Ankit
+
+    {
+        public static void Main()
+        {
+           /* Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("your age is:" + age);*/
+
+            Console.WriteLine ("Enter your First Name:");
+            string Fname = Console.ReadLine();
+            Console.WriteLine ("Enter your Last Name:");
+            string Lname = Console.ReadLine();
+            Console.WriteLine ("Fullname is:" + Fname + " " + Lname);
+            // int age = Convert.ToInt32(Console.ReadLine());
+            // //Console.WriteLine("Your age is: " + age + " and are a voter");
+            // if (age >= 18)
+            // {
+            //     Console.WriteLine("Your age is: " + age + " and are a voter");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Your age is: " + age + " and are not a voter");
+            // }
+
+
+
+            return;  
+        }
+    }
