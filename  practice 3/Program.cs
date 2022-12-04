5efade8592e6eeb9cdc4064b5a0b3725889bb376
d0cdilip <sharmadilip9849@gmail.com>
2022-12-04 16:06:42 +0545

    changed

---  practice 3/Program.cs
+++  practice 3/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World ankit dhunagab!");
+//Console.WriteLine("Hello, World ankit dhunagab!");
+
+Console.WriteLine("Hello, World dilip adhikari!");
 
