fdb7f9c6fcfe97d74ec9fc484e6d9c49d2db169d
ad1995bs <114225958+ad1995bs@users.noreply.github.com>
2022-12-05 18:27:43 +0545

    calculator

---
+++ Loop/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Calculator
+{
+    public static void Main()
+    {
+        Console.WriteLine ("Welcome to our Calculator");
+
+        WhileLoop w = new();
+        w.LearnWhile();
+
+
+        Add a=new();
+        a.Addition();
+       
+       Subtract s = new();
+       s.Subtraction();
+
+       Divide d= new();
+       d.Division();
+       
+
+       Multiply m = new();
+       m.Multiplication();
+
+        
+    }
+}
---
+++ Loop/WhileLoop.cs
@@ -0,0 +1,21 @@
+class WhileLoop
+{
+    public void LearnWhile()
+    {
+    Console.WriteLine("Select the number");
+    int Select =Convert.ToInt16( Console.ReadLine());
+
+    switch (select)
+    {
+
+        case 1:
+        
+
+
+
+    }
+
+        
+    }
+    
+}
