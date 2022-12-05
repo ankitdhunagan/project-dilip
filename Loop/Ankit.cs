e979854be6a5ad286d3e4807261bd9e35947ed3f
d0cdilip <sharmadilip9849@gmail.com>
2022-12-05 12:21:59 +0545

    loop

---
+++ Loop/Ankit.cs
@@ -0,0 +1,18 @@
+class Ankit
+{
+    //yeta try garum call garna class using object, yeta milena
+    // DoWhile Do1 = new();
+    // Do1.LearnDoWhile();
+    public static void Main()
+    {     
+        //yeta try garum call garna class using object
+        DoWhile Do1 = new();
+        Do1.LearnDoWhile();   
+        Console.WriteLine("-------Hello Kathmandu!!------");
+        //yeta try garum call garna class using object
+        While While1 = new();
+        While1.LearnWhile();
+
+
+    }
+}
